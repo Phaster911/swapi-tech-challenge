2af6691da5d91591c363b572e3723776a034803d
Phaster911 <xavier.goncalves.89@gmail.com>
2024-09-05 17:23:37 +0100

    Render home component in app component
    This commit updates the app component to render the home component
    Unit tests were also updated

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,19 +2,20 @@ import { COLORS } from "../../style-constants";
 import styled from "styled-components";
 import { QueryClientProvider } from "@tanstack/react-query";
 import createQueryClient from "../../react-query/create-queryclient";
+import Home from "../home/Home";
 
 const AppWrapper = styled.div`
   height: 100dvh;
   display: flex;
   flex-direction: column;
-  color: ${COLORS.pageBackground};
+  color: ${COLORS.text};
 `;
 
 export default function App() {
   return (
     <AppWrapper>
       <QueryClientProvider client={createQueryClient()}>
-        <div></div>
+        <Home />
       </QueryClientProvider>
     </AppWrapper>
   );
--- src/components/app/__tests__/App.test.tsx
+++ src/components/app/__tests__/App.test.tsx
@@ -1,6 +1,10 @@
 import { render } from "@testing-library/react";
 import App from "../App";
 
+vi.mock("../../home/Home", () => ({
+  default: () => <div>Home</div>,
+}));
+
 describe("<App />", () => {
   it("renders the component", () => {
     const { container } = render(<App />);

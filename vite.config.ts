539b16d4bb3880b1e94b11a0ac24201041045253
Phaster911 <xavier.goncalves.89@gmail.com>
2024-09-05 14:21:15 +0100

    Add required unit test dependencies and configs
    This commit adds all the dependencies required to unit test the app and all the configs

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig(({ mode }) => ({
           }
     ),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+  },
 }));
